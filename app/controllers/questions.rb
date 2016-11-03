9e0cf89a60abb3182cea59328371ee2cbecbfd8a
Asiya Gizatulina & Jason Perlman & Lindsay Kelly & Nan Min <pair+asyaasha.jpperlm.lindsaymkelly.nanutza@devbootcamp.com>
2016-11-03 16:53:35 -0400

    Work in progress

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -6,6 +6,8 @@ end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
+  puts @question.comments.empty?
+  puts "-------------"
   erb :'questions/show'
 end
 
@@ -13,6 +15,21 @@ post '/questions/:id/comments' do
 
   curr_question = Question.find(params[:id])
   new_comment = Comment.new(user: current_user, text: params["text"], commentable: curr_question)
+  if request.xhr?
+    if new_comment.save
+      status 200
+      content_type :json
+      {comment_text: new_comment.text, user_name: new_comment.user.username}.to_json
+    else
+      status 410
+    end
+  end
+end
+
+post '/questions/:id/answers/:answer_id/comments' do
+
+  curr_ans= Answer.find(params[:answer_id])
+  new_comment = Comment.new(user: current_user, text: params["text"], commentable: curr_ans)
   if request.xhr?
     if new_comment.save
       status 200
--- public/js/application.js
+++ public/js/application.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
   $(this).addClass('hidden');
   });
 
+    $(document).on('click', "#answer_comment", function(event){
+    event.preventDefault();
+    $(this).siblings('form').removeClass('hidden');
+    $(this).addClass('hidden');
+    });
+
   $(document).on('submit', ".submitform", function(event){
 
     var temp = $(this)
@@ -23,6 +29,33 @@ $(document).ready(function() {
           COMMENT BY: "+response["user_name"]+"\
       </div>"
       temp.siblings('div.comments').append(text);
+      temp.siblings('button').removeClass('hidden');
+      temp.addClass('hidden');
+      temp.trigger('reset');
+    })
+    .fail(function(response){
+
+    });
+
+  });
+  $(document).on('submit', ".submitform2", function(event){
+
+    var temp = $(this)
+    data = $(this).serialize();
+    url = $(this).attr("action");
+    event.preventDefault();
+
+    $.ajax({
+          url: url,
+          method: "post",
+          data: data
+    }).done(function(response){
+
+      var text = "<div>\
+          AMAZING COMMENT: "+response["comment_text"]+"<br>\
+          COMMENT BY: "+response["user_name"]+"\
+      </div>"
+      temp.siblings('div.comments2').append(text);
       temp.siblings('button').removeClass('hidden');
       temp.addClass('hidden');
       temp.trigger('reset');
