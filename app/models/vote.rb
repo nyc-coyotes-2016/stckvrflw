f8317284de9fc1bab24af50550344d8d61c1a771
Asiya Gizatulina & Jason Perlman & Lindsay Kelly & Nan Min <pair+asyaasha.jpperlm.lindsaymkelly.nanutza@devbootcamp.com>
2016-11-04 16:32:24 -0400

    Doing scary things

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
 
-  validates_uniqueness_of :user_id, :scope => [:voteable]
+  validates_uniqueness_of :user_id, :scope => [:voteable_id, :voteable_type]
 end
