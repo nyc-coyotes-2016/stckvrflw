957b66a7d0f9447a9ccf042b49038709ead6cddf
Asiya Gizatulina & Nan Min <pair+asyaasha.nanutza@devbootcamp.com>
2016-11-04 18:10:21 -0400

    merging files and updating questions controller

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,13 +1,65 @@
-get '/' do
-  @questions = Question.all
-  erb :'questions/index'
+get '/questions/new' do
+  require_login
+  if request.xhr?
+    erb :'_partials/_new_question', layout: false
+  else
+  erb :'questions/new'
+  end
+end
 
+post '/questions' do
+    require_login
+    question = Question.new(params[:question])
+    question[:user_id]=current_user.id
+    if question.save
+      redirect '/'
+    else
+      @errors=question.errors.full_messages
+      erb :'questions/new'
+    end
 end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'questions/show'
 end
+
+get '/questions/:id/edit' do
+  require_login
+  @question = Question.find_by(id: params[:id])
+  erb :'questions/edit'
+end
+
+put '/questions/:id' do
+    require_login
+    # binding.pry
+    @question = Question.find_by(id: params[:id])
+    @question[:user_id]=current_user.id
+    if @question.update(params[:question])
+      redirect '/users/:id'
+    else
+      @errors=question.errors.full_messages
+      erb :'questions/edit'
+    end
+end
+
+delete '/questions/:id' do
+  require_login
+  @question = Question.find_by(id: params[:id])
+  @question.destroy
+  redirect '/users/:id'
+end
+
+# get '/questions/best/:id' do
+#     require_login
+#     @question = Question.find_by(id: params[:id])
+#     @answer = Answer.find_by(id: params[:id])
+#     if owner?
+#     @answer.best = true
+#     redirect "/questions/#{params[:id]}"
+#   end
+# end
+
 
 post '/questions/:id/comments' do
 
