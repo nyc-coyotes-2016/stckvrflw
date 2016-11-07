110dff486dc3bffd5e6e4a4db231ac870df01e47
lindsaymkelly <lindzlady@gmail.com>
2016-11-06 22:47:46 -0500

    Add alternate seeds for Monday presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,38 @@
-bob = User.create(username: "generic", email: "a@gmail.com", password: "password")
-tim = User.create(username: "brother", email: "brother@gmail.com", password: "password")
-laura = User.create(username: "mother", email: "mother@gmail.com", password: "password")
+lindsay = User.create(username: "lindzlady", email: "lindsay@gmail.com", password: "password")
+jason = User.create(username: "jpperlm", email: "jason@gmail.com", password: "password")
+asiya = User.create(username: "asyaasha", email: "asiya@gmail.com", password: "password")
+nan = User.create(username: "nanutza", email: "nan@gmail.com", password: "password")
+generic = User.create(username: "genericperson", email: "generic@gmail.com", password: "password")
+tim = User.create(username: "timmy", email: "tim@gmail.com", password: "password")
+
+question1 = Question.create(title:'Which enumerable should I use?', text:'I am torn between using inject or reduce. Which enumerable is better?', user: generic)
+
+question2 = Question.create(title:'What does ajax mean?', text:'I keep hearing about it in programming, but I thought it was a type of cleaner.', user: asiya)
+
+question3 = Question.create(title:'Should I attend Dev Bootcamp?', text:'I hear that it is a cool place, but I am not sure. Any thoughts?', user: nan)
+
+question4 = Question.create(title:'help please', text:'i am trapped in an infinite loop', user: jason)
+
+question5 = Question.create(title:'Super serious question', text:'How do I put emojis in my commit messages?', user: lindsay)
+
+question6 = Question.create(title:'Which programming language should I learn?', text:'I want to learn, but I cannot pick a language. What should I choose?', user: generic)
 
 
-question1 = Question.create(title:'help please', text:'i am stuck in the mud', user: bob)
-question2 = Question.create(title:'How old are monkeys', text:'its a serious question', user: tim)
-question3 = Question.create(title:'I want to be', text:'I am not', user: laura)
+answer1 = Answer.create(text:'Team Inject!', question:question1, user:lindsay)
+answer2 = Answer.create(text:'Team Reduce!', question:question1, user:tim)
+answer3 = Answer.create(text:'Stands for Asynchronous JavaScript and XML', question:question2, user:nan)
+answer4 = Answer.create(text:'I hear it works great on windows...', question:question2, user:tim)
+answer5 = Answer.create(text:'Yes', question:question3, user:asiya)
+answer6 = Answer.create(text:'Did you forget to increment your loop?', question:question4, user:generic)
+answer7 = Answer.create(text:'My vote is for LOLCode', question:question6, user:jason)
 
 
-answer1 = Answer.create(text:'no', question:question1, user:laura)
-answer2 = Answer.create(text:'no', question:question2, user:tim)
-answer3 = Answer.create(text:'i dont like you question 1', question:question1, user:tim)
 
-answer_comment = Comment.create(text:'answer comment', commentable: answer1, user: tim)
+question_comment = Comment.create(text:'I thought they were the same thing...', commentable: question1, user: nan)
 
-question_comment = Comment.create(text:'question comment', commentable: question1, user: laura)
+answer_comment = Comment.create(text:'Is that a pun?', commentable: answer4, user: lindsay)
 
-vote1 = Vote.create(user:bob, voteable:answer1, vote_direction:1)
-vote1 = Vote.create(user:laura, voteable:answer1, vote_direction:1)
-vote1 = Vote.create(user:tim, voteable:answer1, vote_direction:1)
-
-vote2 = Vote.create(user:laura, voteable:answer2, vote_direction:-1)
+vote1 = Vote.create(user:jason, voteable:answer1, vote_direction:1)
+vote2 = Vote.create(user:lindsay, voteable:answer2, vote_direction:-1)
+vote3 = Vote.create(user:tim, voteable:answer1, vote_direction:1)
+vote4 = Vote.create(user:asiya, voteable:answer2, vote_direction:-1)
